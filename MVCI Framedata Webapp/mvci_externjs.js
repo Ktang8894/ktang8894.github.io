fd0b7cd5bde93fc69d28aa16c29db118bab9b850
Kevin Tang <ktang8894@gmail.com>
2017-10-27 21:43:41 -0500

    Added table and animations

--- MVCI Framedata Webapp/mvci_externjs.js
+++ MVCI Framedata Webapp/mvci_externjs.js
@@ -1,5 +1,36 @@
+function mainModuleAnim() {
+	$("#header").animate({
+		fontSize: '30px',
+		margin: '5'
+	});
+	$("h2").animate({
+		fontSize: '20px',
+		margin: '5'
+	});
+	$(".mainContainer").animate({			
+		padding: '0'
+	});
+	$("select").animate({
+		width: '150px'
+	});
+}
+
 function charSelected(selectId1, selectId2) {
 	if (document.getElementById(selectId1).value == document.getElementById(selectId2).value) {
 		document.getElementById(selectId2).selectedIndex = 0;
 	}
+}
+
+function characterNameAnim() {
+	$("#selectedCharName").fadeOut("fast", function() {
+			var charName = $("#charFrameData").find(":selected").text();
+			$("#selectedCharName").text(charName);
+			$("#selectedCharName").fadeIn("slow");
+	});
+}
+
+function tableLoadAnim() {
+	$("#frameDataContainer").fadeOut("fast", function(){
+		$("#frameDataContainer").slideDown("slow");
+	});
 }
